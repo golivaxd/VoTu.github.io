57b8463d90f451ca8e7a4cefa4cd974a63871968
Goliva <95060883+golivaxd@users.noreply.github.com>
2025-02-26 20:11:14 -0600

    Delete F/app.js

--- F/app.js
+++
@@ -1,6 +0,0 @@
-const backendUrl = process.env.REACT_APP_BACKEND_URL;
-
-fetch(`${backendUrl}/api/usuarios`)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
